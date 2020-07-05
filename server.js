e80417b33b4471d3280b6a8ec42a0b21cd0d914e
mayurnovitekk <mayur.novitekk@gmail.com>
2020-07-05 13:40:52 +0530

    Setup start on heroku port

--- server.js
+++ server.js
@@ -1,7 +1,7 @@
 const express = require('express');
 const hbs = require('hbs');
 const fs = require('fs');
-
+const port = process.env.PORT || 3000;
 
 var app = express();
 app.set('view engine', 'hbs');
@@ -52,4 +52,6 @@ app.get('/about', (req, res) => {
 // app.get('/404', (req, res) => {
 //   res.send('<center><h1>404 Error Page</h1></center>');
 // });
-app.listen(3000);
+app.listen(port, () => {
+  console.log(`Server is up on port ${port}`);
+});
